faba75d129a6bfdc197f059f983efedee4cf906b
ratrap44 <62080086+ratrap44@users.noreply.github.com>
2020-09-08 15:56:50 -0400

    lab1

---
+++ AGGP231Lab01/Assets/Scripts/LandMine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LandMine : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            SceneManager.LoadScene(4);
+            Debug.Log("triggered");
+        }
+    }
+}
