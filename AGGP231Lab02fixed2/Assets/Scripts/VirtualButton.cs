278e464251788dd8cace12699c5e0b0248e1867e
ratrap44 <62080086+ratrap44@users.noreply.github.com>
2020-09-11 13:42:44 -0400

    lab02

---
+++ AGGP231Lab02fixed2/Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject VB;
+    public GameObject Sphere;
+    public Material material;
+    
+    void start()
+    {
+        VB.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        //material = GetComponent<Renderer>().material;
+
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Sphere.SetActive(false);
+        //material.color = Color.red;
+        Debug.Log("VB pressed");
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Sphere.SetActive(true);
+        //material.color = Color.black;
+        Debug.Log("VB released");
+    }
+}
